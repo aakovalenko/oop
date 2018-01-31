4262f68ba6e5028380c2227e90c3ed2c867cee97
andrii <kaa89pl@gmail.com>
2018-01-31 15:54:14 +0200

    added login/reg pages

---
+++ PHP-Login-System/inc/config.php
@@ -0,0 +1,9 @@
+<?php
+
+//if there is no constant defined called __CONFIG__,do not load this file
+    if (!defined('__CONFIG__')) {
+        exit('You do nothave a config file');
+    }
+
+    //our config is bellow
+
--- PHP-Login-System/login.php
+++ PHP-Login-System/login.php
@@ -1,6 +1,6 @@
 <?php
 //allow the config
-    define('__CONFIG__',true);
+define('__CONFIG__',true);
 
 require_once "inc/config.php";
 ?>
@@ -11,19 +11,29 @@ require_once "inc/config.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP-Login-System</title>
+    <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
-    </head>
+</head>
 <body>
 <br><br><br><br><br>
+<h2>Login</h2>
 <div class="container">
-    <p>Hello, today is <?php echo date('Y-m-d');?></p>
-    <a href="./login.php">Login</a>
-    <a href="./register.php">Registration</a>
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="email1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="password" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
 </div>
 </body>
 </html>
