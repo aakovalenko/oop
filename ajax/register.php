ccc5c08ee4941df0b0f0276cd55443b7bff58f38
andrii <kaa89pl@gmail.com>
2018-02-26 10:20:01 +0200

    change html with help of innerHTML

--- ajax/register.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: andrii
- * Date: 01.02.18
- * Time: 12:21
- */
--- index.php
+++ index.php
@@ -1,33 +1,9 @@
 <?php
-//allow the config
-    define('__CONFIG__',true);
 
-require_once "inc/config.php";
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP-Login-System</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+echo 'H1';
 
-    </head>
-<body>
-<br><br><br><br><br>
-<div class="container">
-    <p>Hello, today is: <?php echo date('Y-m-d');?></p>
-    <a href="./login.php">Login</a>
-    <a href="./register.php">Registration</a>
-    <a href="./ajax.php"> Ajax</a>
-</div>
-</body>
-</html>
 
-<script src="/assets/js/main.js"></script>
+
+
+
 
