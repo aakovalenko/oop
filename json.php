2f57bae17275f0be2191d5a9d2a621657525b460
andrii <kaa89pl@gmail.com>
2018-02-26 15:37:10 +0200

    display JSON data

---
+++ json.php
@@ -0,0 +1,10 @@
+<?php
+
+print ("{");
+print("\"firstName\" : \"Mark\",");
+print("\"lastName\" : \"Lassoff\",");
+print("\"company\" : \"LearnToProgram.tv, Incorporated\" ,");
+print("\"age\" : \"36\" ");
+print("}");
+
+?>
